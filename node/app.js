00a0a2dd3eb4b3aab51906929be0c47afa9b9f28
vandna-44 <vandnabaman732@gmail.com>
2025-03-18 21:22:38 +0530

    router

--- node/app.js
+++ node/app.js
@@ -1,8 +1,28 @@
+//1 create server
+//const http = require('http')
+
+//const server = http.createServer((req,res) => {
+ //   res.end('hello world')
+//})
+
+
+//server.listen(3000)
+
+//----------------------
+
+// 2 routing
 const http = require('http')
 
-const server = http.createServer((req,res) => {
-    res.end('hello world')
+const server = http.createServer((req, res) =>{
+    if(req.url == "/about"){
+        res.end("the about page")
+    }
+    if(req.url == "/profile"){
+        res.end("the profile page")
+    }
+    if(req.url=="/"){
+        res.end("the home page")
+    }
 })
-
 
 server.listen(3000)
