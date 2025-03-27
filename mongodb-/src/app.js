b5c6c16bbc20a059f70ff58cbd4eb9aa77f20435
vandna-44 <vandnabaman732@gmail.com>
2025-03-27 14:45:44 +0530

    view-engine

--- mongodb-/src/app.js
+++ mongodb-/src/app.js
@@ -1,7 +1,19 @@
 //sever create krna
 //sever config 
 const express = require('express')
-
 const app = express()
+
+
+app.set('view engine', 'ejs')
+
+app.use((req, res, next) =>{
+    console.log("this is middleware")
+})
+
+app.get('/', (req,res) => {
+    res.render('index')
+})
+
+
 
 module.exports = app;
